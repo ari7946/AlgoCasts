3c6c5f18cff9576fd79cd0a2cc0491ef37464e93
ari7946 <mercado7946@gmail.com>
2019-09-04 14:12:50 -0700

    Complete reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+// function reverse(str) {
+//   var newString = "";
+//   for (var i = str.length - 1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+//   return newString;
+// }
+function reverse(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed
+}
+
+
 
 module.exports = reverse;
