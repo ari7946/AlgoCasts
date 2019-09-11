4c8adf310f368de52f1eb06c3f98d3d71a4f94cb
ari7946 <mercado7946@gmail.com>
2019-09-11 14:23:59 -0700

    Solve chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const arr = [];
+  array.forEach(el => {
+    const last = arr[arr.length - 1];
+
+    if (!last || last.length == size) {
+      arr.push([el])
+    } else {
+      last.push(el)
+    }
+  })
+  return arr;
+}
 
 module.exports = chunk;
