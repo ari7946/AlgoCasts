c8bde7473269cd3425fe3bcc49a4e40178e21ab3
ari7946 <mercado7946@gmail.com>
2019-09-14 19:04:16 -0700

    Solves anagrams, brute force

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,48 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+//* creates character map
+// function maxChar(str) {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = '';
 
-function anagrams(stringA, stringB) {}
+//   for (let char of str) {
+//     // chars[char] = chars[char] + 1 || 1
+//     charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar
+// }
+
+function anagrams(stringA, stringB) {
+stringA = stringA.replace(/[^\w]/g, '').toLowerCase().split('');
+stringB = stringB.replace(/[^\w]/g, '').toLowerCase().split('');
+if (stringA.length !== stringB.length) {
+  return false
+}
+let count = 0;
+for (let charA of stringA) {
+  var found = false;
+
+
+  for (let charB of stringB) {
+    if (!found) {
+      if (charA === charB) {
+        count++;
+        found = true
+      }
+    }
+  }
+  found = false;
+}
+return count == stringA.length ? true : false;
+}
 
 module.exports = anagrams;
