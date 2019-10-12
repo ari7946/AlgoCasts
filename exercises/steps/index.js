327eebcee73983830b4444a2525a1b308dccbab3
ari7946 <mercado7946@gmail.com>
2019-10-11 20:24:32 -0700

    Small change to forEach

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -27,8 +27,9 @@ function steps(n) {
     initialValues[i] = '#';
     return initialValues.join('');
   })
-  steps.forEach(hey => console.log(hey));
+  steps.forEach(step => console.log(step));
 }
-
+//! pythontutor for above solution
+// http://www.pythontutor.com/live.html#code=function%20steps%28n%29%20%7B%0A%20%20const%20steps%20%3D%20%5B%5D%3B%0A%20%20while%20%28n%29%20%7B%0A%20%20%20%20steps.push%28%22%20%22%29%3B%0A%20%20%20%20n--%0A%20%20%7D%0A%20%20return%20steps%0A%20%20%20%20.map%28%28step,%20i%29%20%3D%3E%20%7B%0A%20%20%20%20%20%20steps%5Bi%5D%20%3D%20'%23'%3B%0A%20%20%20%20%20%20return%20steps.join%28''%29%3B%0A%20%20%20%20%7D%29%0A%20%20%20%20.forEach%28hey%20%3D%3E%20console.log%28hey%29%29%3B%0A%7D%0A%0Asteps%283%29%3B&cumulative=false&curInstr=30&heapPrimitives=nevernest&mode=display&origin=opt-live.js&py=js&rawInputLstJSON=%5B%5D&textReferences=false
 
 module.exports = steps;
