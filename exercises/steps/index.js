2dcfdefa192164ae009e0540e7f7b7ed1fbfd503
ari7946 <mercado7946@gmail.com>
2019-10-12 18:30:24 -0700

    Include pythontutor link

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  let initialValues = [];
-  while (n) {
-    initialValues.push(" ");
-    n--
-  }
-  const steps = initialValues.map((step, i) => {
-    initialValues[i] = '#';
-    return initialValues.join('');
-  })
-  steps.forEach(step => console.log(step));
-}
+// function steps(n) {
+//   let initialValues = [];
+//   while (n) {
+//     initialValues.push(" ");
+//     n--
+//   }
+//   initialValues.forEach((step, i) => {
+//     initialValues[i] = '#';
+//     console.log(initialValues.join(''));
+//   })
+
+// }
 //! pythontutor for above solution
 // http://www.pythontutor.com/live.html#code=function%20steps%28n%29%20%7B%0A%20%20const%20steps%20%3D%20%5B%5D%3B%0A%20%20while%20%28n%29%20%7B%0A%20%20%20%20steps.push%28%22%20%22%29%3B%0A%20%20%20%20n--%0A%20%20%7D%0A%20%20return%20steps%0A%20%20%20%20.map%28%28step,%20i%29%20%3D%3E%20%7B%0A%20%20%20%20%20%20steps%5Bi%5D%20%3D%20'%23'%3B%0A%20%20%20%20%20%20return%20steps.join%28''%29%3B%0A%20%20%20%20%7D%29%0A%20%20%20%20.forEach%28hey%20%3D%3E%20console.log%28hey%29%29%3B%0A%7D%0A%0Asteps%283%29%3B&cumulative=false&curInstr=30&heapPrimitives=nevernest&mode=display&origin=opt-live.js&py=js&rawInputLstJSON=%5B%5D&textReferences=false
+
+
+//* Solution using nested for loops
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+
+    console.log(stair)
+  }
+}
+//! pythontutor for the above solution
+// http://www.pythontutor.com/javascript.html#code=function%20steps%28n%29%20%7B%0A%20%20for%20%28let%20row%20%3D%200%3B%20row%20%3C%20n%3B%20row%2B%2B%29%20%7B%0A%20%20%20%20let%20stair%20%3D%20''%3B%0A%0A%20%20%20%20for%20%28let%20column%20%3D%200%3B%20column%20%3C%20n%3B%20column%2B%2B%29%20%7B%0A%20%20%20%20%20%20if%20%28column%20%3C%3D%20row%29%20%7B%0A%20%20%20%20%20%20%20%20stair%20%2B%3D%20'%23'%3B%0A%20%20%20%20%20%20%7D%20else%20%7B%0A%20%20%20%20%20%20%20%20stair%20%2B%3D%20'%20'%3B%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%20%20%0A%20%20%20%20console.log%28stair%29%0A%20%20%7D%0A%7D%0A%0Asteps%283%29%3B&curInstr=57&mode=display&origin=opt-frontend.js&py=js&rawInputLstJSON=%5B%5D
 
 module.exports = steps;
