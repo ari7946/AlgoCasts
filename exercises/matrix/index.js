2fa593152b2517a4b4545c8f117c50b4c822d16a
ari7946 <mercado7946@gmail.com>
2019-10-15 17:25:12 -0700

    Attempt to solve by any means and it's pretty bad.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -21,13 +21,12 @@ function matrix(n) {
 
   let num = n;
   while (num) {
-    arr.push("");
+    arr.push(" ");
     num--;
   }
 
   const matrix = arr.map((el, i) => {
     let counter1 = n + (n - 1);
-
     return arr.map((ele, ii) => {
       //last array
       if (i + 1 === n) {
@@ -44,9 +43,32 @@ function matrix(n) {
   if (matrix.length - 2 === 1) {
     matrix[1][0] = matrix[2][0] + 1
     matrix[1][1] = matrix[1][0] + 1
-    matrix[1][2] = matrix[0][2] + 1
+    matrix[1][2] = matrix[0][2] + 1;
+  // } else if (n >= 4) {
+  //   const lastIndex = matrix.length - 1;
+  //   const sliced = matrix.slice(1, lastIndex);
+  //   const firstRow = matrix[0];
+  //   const lastRow = matrix[lastIndex];
+  //   const numRows = sliced.length;
+  //   for (let i = 0; i < numRows; i++) {
+  //     for (let j = 0; j < sliced[i].length; j++) {
+  //       console.log('j', matrix[i][j]);
+  //       sliced[i][0] = lastRow[i] + numRows;
+  //     }
+  //   }
+
+  //   let count = 0;
+  //   while (numRows >= count) {
+  //     sliced[0][count] = lastRow[0] + numRows + count;
+  //     sliced[0][matrix.length - 1] = lastRow[lastRow.length - 1] - numRows
+
+  //     sliced[1][count ? count : null] = total;
+  //     sliced[1][matrix.length - 1] = lastRow[lastRow.length - 1] - numRows + 1
+  //     count++;
+  //   }
+  //   sliced[1][numRows] = total - 1;
   }
-  
+
   return matrix;
 }
 
