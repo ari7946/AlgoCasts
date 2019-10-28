c3a5ff7c70383e36db3b2d23a7d6c8e83d415d7c
ari7946 <mercado7946@gmail.com>
2019-10-28 16:13:34 -0700

    Changes to tree

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counters = [0];
+  const stopper = 's';
+  const arr = [root, stopper];
+
+  while (arr.length > 1) {
+    if (arr[0] === stopper) {
+      counters.push(0);
+      let node = arr.shift();
+      arr.push(node);
+    } else {
+      let node = arr.shift();
+      arr.push(...node.children);
+      counters[counters.length - 1] += 1;
+    }
+  }
+  
+  return counters; 
+}
 
 module.exports = levelWidth;
