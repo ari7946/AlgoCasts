0dfe1163790471e09eb00749f7197ed4d123ac00
ari7946 <mercado7946@gmail.com>
2019-10-30 15:19:07 -0700

    Add comments to bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,6 +2,10 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  // For each iteration of the outer loop, inner loop is shortened by 1;
+  // if element at j is greater than the element at j + 1, swap them.
+  // Iterations will continue to run even after the array has been sorted.
+  // n^2 runtime complexity.
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
